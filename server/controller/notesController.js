e75dd86dfb2b1ea99dade99d830feda0d7259701
JustaDoraemonfan <souvikkpatra06@gmail.com>
2025-11-26 18:41:47 +0530

    Some random slice changes

--- server/controller/notesController.js
+++ server/controller/notesController.js
@@ -81,7 +81,7 @@ export const updateNote = async (req, res) => {
       return res.status(404).json({ message: "Note not found" });
     }
 
-    res.status(200).json(note);
+    res.(200).json(note);
   } catch (error) {
     console.error("Error updating note:", error);
     res.status(500).json({ message: "Server error while updating note" });
